7b506eadc9397471eddf45777699cbb5876305de
Michael P-F <mikeyredtiger22@gmail.com>
2018-10-10 10:45:22 +0100

    Added ESLint.

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define,react/prefer-stateless-function */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -6,14 +7,16 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import {
+  Platform, StyleSheet, Text, View,
+} from 'react-native';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  ios: 'Press Cmd+R to reload,\n'
+    + 'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n'
+    + 'Shake or press menu button for dev menu',
 });
 
 type Props = {};
--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -5,7 +5,7 @@ jasmine.DEFAULT_TIMEOUT_INTERVAL = 100 * 1000;
 const capabilities = {
   platformName: 'Android',
   deviceName: 'Android Emulator',
-  app: '/Users/michael/Documents/appium-test/my-appium-test-proj/AppiumTest/android/app/build/outputs/apk/release/app-release.apk'
+  app: '/Users/michael/Documents/appium-test/my-appium-test-proj/AppiumTest/android/app/build/outputs/apk/release/app-release.apk',
 };
 
 const driver = wd.promiseChainRemote('localhost', 4723);
@@ -14,8 +14,7 @@ describe('App', () => {
   beforeAll(async () => {
     try {
       await driver.init(capabilities);
-    }
-    catch(error) {
+    } catch (error) {
       console.error(error);
     }
   });
@@ -23,8 +22,7 @@ describe('App', () => {
   afterAll(async () => {
     try {
       await driver.quit();
-    }
-    catch(error) {
+    } catch (error) {
       console.error(error);
     }
   });
@@ -32,5 +30,44 @@ describe('App', () => {
   it('app renders', async () => {
     expect(await driver.hasElementByAccessibilityId('testview')).toBe(true);
     expect(await driver.hasElementByAccessibilityId('notthere')).toBe(false);
+    // expect(await driver.hasElementByAccessibilityId('counter')).toBe(true);
+    // expect(await driver.hasElementByAccessibilityId('counterInc')).toBe(true);
   });
+
+
+  // it('appium button click', async () => {
+  //   let counterIncButton = await driver.elementByAccessibilityId('counterInc');
+  //   counterIncButton.tap();
+  //   counterIncButton.tap();
+  // });
+  //
+  // it('appium button click', async () => {
+  //   // expect(await driver.hasElementByAccessibilityId('counter')).toBe(true);
+  //   let counter = await driver.elementByAccessibilityId('counter');
+  //   expect(await counter.text()).toBe('Counter: 2');
+  // });
+
+  // it('should render correctly', async () => {
+  //   expect(await driver.hasElementByAccessibilityId('notthere')).toBe(false);
+  //   expect(await driver.hasElementByAccessibilityId('testview')).toBe(true);
+  // });
+  //
+  // it('should see more', async () => {
+  //   expect(await driver.hasElementByAccessibilityId('notthere')).toBe(false);
+  //   expect(await driver.hasElementByAccessibilityId('testview')).toBe(true);
+  //   expect(await driver.hasElementByAccessibilityId('counterInc')).toBe(true);
+  //   expect(await driver.hasElementByAccessibilityId('counter')).toBe(true);
+  // });
+  //
+  // it('click', async () => {
+  //   let el1 = await driver.elementByAccessibilityId("counterInc");
+  //   await el1.click();
+  //   await el1.tap();
+  //   let el2 = await driver.elementByXPath('//android.widget.Button[@content-desc="counterInc"]');
+  //   await el2.tap();
+  //   await el2.click();
+  //   expect(await driver.hasElementByAccessibilityId('counter')).toBe(true);
+  //   expect(await driver.hasElementByAccessibilityId('testview')).toBe(true);
+  //   expect(await driver.hasElementByAccessibilityId('counterInc')).toBe(true);
+  // });
 });
