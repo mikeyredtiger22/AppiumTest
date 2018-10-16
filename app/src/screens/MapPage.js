899fae7741ccb64d3bae7f4bac6c0c6055f79976
Michael P-F <mikeyredtiger22@gmail.com>
2018-10-16 16:23:55 +0100

    Map markers have accessibility labels and custom callout views.

--- app/src/screens/MapPage.js
+++ app/src/screens/MapPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, View, Text, Dimensions, TouchableOpacity } from 'react-native';
-import MapView, { Marker, PROVIDER_DEFAULT } from 'react-native-maps';
+import MapView, { Marker, Callout, PROVIDER_DEFAULT } from 'react-native-maps';
+import MapMarkerCallout from '../components/MapMarkerCallout';
 
 const { width, height } = Dimensions.get('window');
 
@@ -9,7 +10,7 @@ const LATITUDE = 37.78825;
 const LONGITUDE = -122.4324;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
-let id = 0;
+const SPACE = 0.01;
 
 function randomColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
@@ -26,46 +27,86 @@ class DefaultMarkers extends React.Component {
         latitudeDelta: LATITUDE_DELTA,
         longitudeDelta: LONGITUDE_DELTA,
       },
-      markers: [],
+      markers: [
+        {
+          coordinate: {
+            latitude: LATITUDE + SPACE,
+            longitude: LONGITUDE + SPACE,
+          },
+        },
+        {
+          coordinate: {
+            latitude: LATITUDE,
+            longitude: LONGITUDE,
+          },
+        },
+        {
+          coordinate: {
+            latitude: LATITUDE + SPACE,
+            longitude: LONGITUDE - SPACE,
+          },
+        },
+      ],
     };
   }
 
-  onMapPress(e) {
-    this.setState(prevState => ({
-      markers: [
-        ...prevState.markers,
-        {
-          coordinate: e.nativeEvent.coordinate,
-          key: id++,
-          color: randomColor(),
-        },
-      ],
-    }));
+  show() {
+    this.marker1.showCallout();
+  }
+
+  hide() {
+    this.marker1.hideCallout();
   }
 
   render() {
+    const { region, markers } = this.state;
     return (
       <View style={styles.container}>
         <MapView
           provider={PROVIDER_DEFAULT}
           style={styles.map}
-          initialRegion={this.state.region}
-          onPress={e => this.onMapPress(e)}
+          initialRegion={region}
         >
-          {this.state.markers.map(marker => (
-            <Marker
-              key={marker.key}
-              coordinate={marker.coordinate}
-              pinColor={marker.color}
-            />
-          ))}
+          <Marker
+            accessibilityLabel="Label0"
+            coordinate={markers[0].coordinate}
+            title="Title"
+            description="Desc"
+          >
+            <Callout tooltip style={styles.customView}>
+              <MapMarkerCallout>
+                <Text>This is a custom callout bubble view</Text>
+              </MapMarkerCallout>
+            </Callout>
+          </Marker>
+          <Marker
+            accessibilityLabel="Label1"
+            coordinate={markers[1].coordinate}
+            title="Title"
+            description="Desc"
+          >
+            <Callout tooltip style={styles.customView}>
+              <MapMarkerCallout>
+                <Text>This is a custom callout bubble view</Text>
+              </MapMarkerCallout>
+            </Callout>
+          </Marker>
         </MapView>
         <View style={styles.buttonContainer}>
+          <View style={styles.bubble}>
+            <Text>Tap on markers to see different callouts</Text>
+          </View>
+        </View>
+        <View style={styles.buttonContainer}>
           <TouchableOpacity
-            onPress={() => this.setState({ markers: [] })}
-            style={styles.bubble}
+            onPress={() => this.show()}
+            style={[styles.bubble, styles.button]}
+            accessibilityLabel="ShowCallout"
           >
-            <Text>Tap to create a marker of random color</Text>
+            <Text>Show</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => this.hide()} style={[styles.bubble, styles.button]}>
+            <Text>Hide</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -74,6 +115,13 @@ class DefaultMarkers extends React.Component {
 }
 
 const styles = StyleSheet.create({
+  customView: {
+    width: 140,
+    height: 100,
+  },
+  plainView: {
+    width: 60,
+  },
   container: {
     ...StyleSheet.absoluteFillObject,
     justifyContent: 'flex-end',
@@ -83,6 +131,7 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
   },
   bubble: {
+    flex: 1,
     backgroundColor: 'rgba(255,255,255,0.7)',
     paddingHorizontal: 18,
     paddingVertical: 12,
